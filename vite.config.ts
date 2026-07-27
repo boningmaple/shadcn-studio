5cbf1a74cdc434f87ebbfb68d02a69db0b0c7b62
boning <boningmaple@gmail.com>
2026-07-27 17:09:36 +1000

    init with Vite+, TanStack Start

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig, lazyPlugins } from "vite-plus";
+import { devtools } from "@tanstack/devtools-vite";
+
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
+
+const config = defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {
+    printWidth: 80,
+    ignorePatterns: ["routeTree.gen.ts"],
+  },
+  lint: {
+    jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+    rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+    options: { typeAware: true, typeCheck: true },
+  },
+  resolve: { tsconfigPaths: true },
+  plugins: lazyPlugins(() => [
+    devtools(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ]),
+});
+
+export default config;
